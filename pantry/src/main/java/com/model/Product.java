c44aa51c460f47b47ab15240b3a4660a63f13a53
Dhruv Patel <dhruvizawesom19@gmail.com>
2026-01-27 10:20:21 -0500

    Created User and Product model object classes

--- pantry/src/main/java/com/model/Product.java
+++ pantry/src/main/java/com/model/Product.java
@@ -1,5 +1,44 @@
 package com.model;
 
 public class Product {
-    
+    private int id;
+    private static int idCounter = 0;
+    private int quantity;
+    private String name;
+    private String expirationDate;
+
+    public Product(String name, int quantity, String expirationDate) {
+        this.id = idCounter++;
+        this.name = name;
+        this.quantity = quantity;
+        this.expirationDate = expirationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
---
+++ pantry/src/main/java/com/model/User.java
@@ -0,0 +1,59 @@
+package com.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private String username;
+    private String email;
+    // private String passwordHashString;
+    private static int idCounter = 1;
+    private final int id;
+    private LocalDateTime createdAt;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.id = idCounter++;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
