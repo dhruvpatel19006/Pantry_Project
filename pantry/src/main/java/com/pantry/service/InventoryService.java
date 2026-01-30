4cf26023138f754c8064b87678f6ab138f5ca466
Dhruv Patel <dhruvizawesom19@gmail.com>
2026-01-30 14:40:46 -0500

    Finished inventory service file for now prototyped

--- pantry/src/main/java/com/pantry/service/InventoryService.java
+++ pantry/src/main/java/com/pantry/service/InventoryService.java
@@ -1,5 +1,53 @@
 package com.pantry.service;
 
+
+
+import com.pantry.model.UserItem;
+import com.pantry.repository.InventoryRepository;
+import com.pantry.util.ExpirationUtils;
+import java.util.List;
+import java.util.ArrayList;
+
 public class InventoryService {
-    
+    private final InventoryRepository repository;
+
+    public InventoryService(InventoryRepository repository){
+        this.repository = repository;
+    }
+
+    public void addItem(UserItem item){
+        repository.addItem(item);
+    }
+
+    public List<UserItem> getAllItems(){
+        return repository.findAll();
+    }
+
+    public List<UserItem> getExpiredItems(){
+        List<UserItem> allItems = getAllItems();
+        List<UserItem> expiredItems = new ArrayList<>();
+        for (UserItem item : allItems) {
+            if (ExpirationUtils.isExpired(item)) {
+                expiredItems.add(item);
+            }
+        }
+        return expiredItems;
+    }
+
+    public List<UserItem> getItemsExpiringWithinDays(int days){
+        List<UserItem> allItems = getAllItems();
+        List<UserItem> expiringItems = new ArrayList<>();
+        for (UserItem item : allItems) {
+            if (ExpirationUtils.expiresWithinDays(item, days)) {
+                expiringItems.add(item);
+            }
+        }
+        return expiringItems;
+    }
+
+    public List<UserItem> getItemsSortedByExpiration(){
+        List<UserItem> allItems = getAllItems();
+        allItems.sort((item1, item2) -> item1.getExpirationDate().compareTo(item2.getExpirationDate()));
+        return allItems;
+    }
 }
