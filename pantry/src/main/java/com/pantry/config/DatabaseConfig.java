e27e492fce37c58729f88c26ad7ee49a799e52b4
Dhruv Patel <dhruvizawesom19@gmail.com>
2026-01-30 19:54:59 -0500

    Created the .db file and the .sql file as well as the databaseconfig class

--- pantry/src/main/java/com/pantry/config/DatabaseConfig.java
+++ pantry/src/main/java/com/pantry/config/DatabaseConfig.java
@@ -1,5 +1,42 @@
 package com.pantry.config;
 
-public class DatabaseConfig {
-    
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class DatabaseConfig{
+    private static final String URL = "jdbc:sqlite:pantry.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+    public static void initializeDatabase(){
+        try (Connection conn = getConnection()) {
+            try(Statement pragma = conn.createStatement()){
+                pragma.execute("PRAGMA foreign_keys = ON;");
+            }
+            runSchema(conn);
+            System.out.println("Database initialized successfully.");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize database", e);
+        }
+    }
+
+    public static void runSchema(Connection conn) throws Exception {
+        InputStream inputStream = DatabaseConfig.class.getClassLoader().getResourceAsStream("schema.sql");
+        if (inputStream == null) {
+            throw new RuntimeException("schema.sql not found in classpath");
+        }
+        String sql = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
